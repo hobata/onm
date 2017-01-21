66bcca5cd6d968a83be01994f72fbc3daf89bcc5
root <root@debian2>
2017-01-21 13:35:09 +0900

    start mesh with no error

--- start-mesh.sh
+++ start-mesh.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -x
+# Wi-Fi mesh for WI-U2-300D
 # refer to https://github.com/o11s/open80211s/wiki/HOWTO
 
 MESH_IFACE=mesh1
@@ -8,16 +9,10 @@ iw list
 ifconfig wlan0 down
 
 sleep 5
-iw dev wlan0 interface add $MESH_IFACE type mp
-ifconfig $MESH_IFACE
+iw dev wlan0 interface add $MESH_IFACE type mp mesh_id $MESH_ID
 sleep 5
 ifconfig $MESH_IFACE 192.168.3.80
-# SIOCSIFFLAGS:resource busy but the interface has the adress
 ifconfig $MESH_IFACE | grep inet
-
-sleep 5
-iw dev $MESH_IFACE mesh join $MESH_ID
-# command failed: Operation not supported (-95)
 
 sleep 5
 # monitor
--- stop-mesh.sh
+++ stop-mesh.sh
@@ -5,7 +5,7 @@ MESH_IFACE=mesh1
 MESH_ID=mesh_test
 
 # down
-#iw dev mesh1 leave
-iw dev mesh1 del 
+#iw dev $MESH_IFACE leave
+iw dev $MESI_ID del 
 ifconfig wlan0 up
 # Sometimes: disconnect and connect wi-fi device
