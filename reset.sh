e7941bbda662c4e4379002d3e085307d5088d5e9
root <root@debian2>
2017-02-04 22:40:24 +0900

    add reset.sh

---
+++ reset.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+iw dev mesh1 del
+service NetworkManager start
--- smesh.py
+++ smesh.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python2.7
 # Start Wi-Fi mesh
 #
-import commands, time, sys
+import commands, sys
+from time import sleep
 
 meshIf = "mesh1"
 bssId= "mesh_test"
@@ -10,27 +11,30 @@ meshAddr= "192.168.55."
 # stop network service
 commands.getoutput("service NetworkManager stop")
 commands.getoutput("killall wpa_supplicant")
-
 # get wlanId 
 wlanId = ""
+mac = ""
 str = commands.getoutput("ifconfig -a")
 sList = str.split("\n")
 for line in sList:
-  mac = line[-2:]
+  if not "wlan" in line:
+    continue
   elem = line.split(" ")
-  for s in elem:
-    if "wlan" in s:
-      wlanId = s
+  wlanId = elem[0]
+  elem = line.split(":")
+  mac = elem[-1]
+  break
 if len(wlanId) < 5:
   print "Can not find wlan IF."
   sys.exit()
+print "mac:%s" % (mac)
 lastAddr = int(mac, 16) # last byte of mac addr
 
 commands.getoutput("ifconfig %s down" % wlanId)
-commands.getoutput("iw reg set JP"
-time.sleep(5)
-commands.getoutput("iw dev %s interface add %s type mp" % (wlanId, meshIf)
-time.sleep(5)
+commands.getoutput("iw reg set JP")
+sleep(5)
+commands.getoutput("iw dev %s interface add %s type mp" % (wlanId, meshIf))
+sleep(5)
 commands.getoutput("ifconfig %s down" % (wlanId))
 commands.getoutput("ifconfig %s %s%d" % (meshIf, meshAddr, lastAddr))
 
